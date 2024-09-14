0b43c904c2b49b09587a836417cca6582cf667a2
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-14 23:47:34 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+export { setup } from "./setup";
+export { generateAccount } from "./account";
+export { generateCommitment } from "./commitment";
+export { getDepositProof, GetDepositProofReturn } from "./get-deposit-proof";
+export { getPoseidon, Poseidon } from "./get-poseidon";
+export { bigIntToHex } from "./utils/big-int-to-hex";
+export { hexToBigInt } from "./utils/hex-to-big-int";
+export { generateProof } from "./generate-proof";
+export { snarkjs, Proof, Protocols, Curves, Logger, PublicSignals, Groth16, SnarkJS } from "./snarkjs";
