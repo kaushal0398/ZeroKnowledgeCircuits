93422fe3ee0c61ef9b694870ebfdc2896914fb3d
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-18 22:44:17 +0530

    updated

--- get-poseidon.ts
+++
@@ -1,9 +0,0 @@
-import { buildPoseidon } from "circomlibjs";
-
-export type Poseidon = (input: (bigint | number)[]) => bigint;
-
-export async function getPoseidon(): Promise<Poseidon> {
-  const originalPoseidon = await buildPoseidon();
-
-  return (input: (bigint | number)[]) => originalPoseidon.F.toObject(originalPoseidon(input));
-}
