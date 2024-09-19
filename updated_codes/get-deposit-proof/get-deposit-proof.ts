c3f859e1ad8bd5c839fc07a9971d5ce0b800b8da
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-19 21:22:20 +0530

    updated

---
+++ updated_codes/get-deposit-proof/get-deposit-proof.ts
@@ -0,0 +1,39 @@
+import { initialize } from "zokrates-js/node";
+import { proveCircuit } from "../prove-circuit";
+import { numToZok } from "../utils/num-to-zok";
+import pkJSON from "../setup/deposit-circuit/pk.json";
+import programJSON from "../setup/deposit-circuit/program.json";
+import abiJSON from "../setup/deposit-circuit/abi.json";
+import { Proof, ProofPoints } from "zokrates-js";
+
+interface Props {
+  commitmentDigest: BigInt;
+  nonce: BigInt;
+  amount: number;
+  ownerDigest: BigInt;
+}
+
+export interface GetDepositProofReturn {
+  proof: ProofPoints;
+  inputs: string[];
+}
+
+export async function getDepositProof({
+  commitmentDigest,
+  nonce,
+  amount,
+  ownerDigest,
+}: Props): Promise<GetDepositProofReturn> {
+  const provider = await initialize();
+
+  const params = [numToZok(commitmentDigest), numToZok(nonce), numToZok(amount), numToZok(ownerDigest)];
+
+  const { proof } = await proveCircuit({
+    provider,
+    params,
+    artifacts: { program: new Uint8Array(Object.values(programJSON.program)), abi: abiJSON.abi },
+    keypairPK: new Uint8Array(Object.values(pkJSON.pk)),
+  });
+
+  return proof;
+}
