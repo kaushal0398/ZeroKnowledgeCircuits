23e64904c07bece6b4ca1f58b1fcd37e471f21b3
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-19 21:15:01 +0530

    updated

---
+++ updated_codes/account/account.ts
@@ -0,0 +1,16 @@
+import { Poseidon } from "../get-poseidon";
+import { hexToBigInt } from "../utils/hex-to-big-int";
+
+export interface Account {
+  account_digest_preimage_0: bigint;
+  account_digest_preimage_1: bigint;
+  account_digest: bigint;
+}
+
+export function generateAccount(poseidon: Poseidon, seed: [number, number]): Account {
+  const account_digest_preimage_0 = hexToBigInt(seed[0].toString(16));
+  const account_digest_preimage_1 = hexToBigInt(seed[1].toString(16));
+  const account_digest = poseidon([account_digest_preimage_0, account_digest_preimage_1]);
+
+  return { account_digest_preimage_0, account_digest_preimage_1, account_digest };
+}
