8a2947a0be4df9e8aa5b6bce4ca15ef16f3cac3a
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2025-03-03 23:36:57 +0530

    Update get-levels.ts

--- merkle-tree/get-levels.ts
+++ merkle-tree/get-levels.ts
@@ -2,7 +2,8 @@ import { Poseidon } from "./get-poseidon";
 
 export function getLevels(poseidon: Poseidon, leafs: bigint[], nrOfDigestsPerDigest: number): bigint[][] {
   const digests = leafs;
-  
+  const levels = [digests];
+  let currentLevel = 0;
   let rootDigestReached = false;
 
   while (rootDigestReached === false) {
